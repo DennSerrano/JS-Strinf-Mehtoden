94cc28587805ddc601e584a33935bac1a352ed33
Dennserrano <dennserrano@gmail.com>
2021-10-25 15:11:05 +0200

    String

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+let x = "Hello"
+let y = "World<br>"
+
+let z = x+y
+
+document.write(z)
+
+let n = x + " " + y
+document.write(n)
+
+world = "World"
+document.write(x + world)
+
+
+let meinString = "Ich bin Erster   "
+
+platzZwei = "Ich komme auf Platz zwei"
+
+document.write(meinString + platzZwei)
